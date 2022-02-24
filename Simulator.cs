a988152cc9c2a30c7a84f62b314bd04d2fee499a
Zaai90 <erik.grune@gmail.com>
2022-02-24 15:48:50 +0100

    Changed to log the simulation, instead of console messages.

--- Simulator.cs
+++ Simulator.cs
@@ -1,55 +1,58 @@
 // See https://aka.ms/new-console-template for more information
-internal class Simulator
+public class Simulator
 {
     public int Leaves { get; set; }
     public int Meats { get; set; }
     public int Days { get; set; } = 0;
 
-    private List<Animal> Animals { get; set; }
+    public string logPath = @"logs/";
+    private List<Animal> Animals { get; set; } = new();
 
-    public Simulator(int leaves, int meats, List<Animal> animals)
+    public Simulator(int leaves, int meats)
     {
+        System.IO.Directory.CreateDirectory("logs/");
         Leaves = leaves;
         Meats = meats;
-        Animals = animals;
+    }
+
+    public void AddAnimal(Animal animal)
+    {
+        Animals.Add(animal);
     }
 
     public void Run()
     {
+        logPath += $"log_{DateTime.Now.Year}-{DateTime.Now.Month}-{DateTime.Now.Day}_{DateTime.Now.Hour}{DateTime.Now.Minute}{DateTime.Now.Second}.txt";
+        File.AppendAllText(logPath, $"Simulation at {DateTime.Now} - Started:");
         DateTime date = DateTime.Now;
 
         while (Leaves! > 0 || Meats! > 0)
         {
-            Console.Clear();
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine($"Food supply levels:\nMeat: {Meats}\nLeaves: {Leaves}");
-            Console.ResetColor();
+            File.AppendAllText(logPath, $"\nFood supply levels:\nMeat: {Meats}\nLeaves: {Leaves}");
 
             date = date.AddDays(1);
-            Console.WriteLine($"It's day {Days}. A beautiful {date.DayOfWeek}.");
+            File.AppendAllText(logPath, $"\n\nIt's day {Days}. A beautiful {date.DayOfWeek}.");
             AnimalLoop(CheckHunger);
 
-            Thread.Sleep(100);
+            Thread.Sleep(10);
             AnimalLoop(IncreaseHunger);
             Days++;
         }
 
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine("\nTimes the animals were fed:");
-        Console.ResetColor();
-        AnimalLoop((animal) => Console.WriteLine($"{animal.Name} the {animal.GetType()} was fed {animal.TimesFed} times."));
-        Console.WriteLine();
-        AnimalLoop((animal) => Console.WriteLine($"{animal.Name} the {animal.GetType()} needs to be fed in: {(animal.HungerLimit - animal.CurrentHunger) + 1} days."));
-        Console.WriteLine($"\nFood ran out after {Days - 1} days. You need to order more {(Leaves >= 0 && Meats <= 0 ? "of both leaves and meat" : Leaves <= 0 ? "leaves" : "meat")}!");
+
+        File.AppendAllText(logPath, "\n\nTimes the animals were fed:");
+        AnimalLoop((animal) => File.AppendAllText(logPath, $"\n{animal.Name} the {animal.GetType()} was fed {animal.TimesFed} times."));
+        File.AppendAllText(logPath, "\n");
+        AnimalLoop((animal) => File.AppendAllText(logPath, $"\n{animal.Name} the {animal.GetType()} needs to be fed in: {(animal.HungerLimit - animal.CurrentHunger) + 1} days."));
+        File.AppendAllText(logPath, $"\nFood ran out after {Days - 1} days. You need to order more {(Leaves >= 0 && Meats <= 0 ? "of both leaves and meat" : Leaves <= 0 ? "leaves" : "meat")}!");
+        File.AppendAllText(logPath, "\n\n---------------------...Simulation ended...------------------------\n");
     }
 
     private void CheckHunger(Animal animal)
     {
         if (animal.IsHungry())
         {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"{animal.Name} the {animal.GetType()} is hungry and needs to eat!");
-            Console.ResetColor();
+            File.AppendAllText(logPath, $"\n{animal.Name} the {animal.GetType()} is hungry and needs to eat!");
             FeedAnimal(animal);
         }
     }
@@ -79,15 +82,13 @@ internal class Simulator
             givenfood = "meat";
             Meats--;
         }
-        Console.WriteLine($"{animal.Name} was fed with some delicious {givenfood}.");
+        File.AppendAllText(logPath, $"\n{animal.Name} was fed with some delicious {givenfood}.");
         animal.Eat();
-
     }
     private void IncreaseHunger(Animal animal)
     {
         animal.IncreaseHunger();
     }
-
 
     private void AnimalLoop(Action<Animal> action)
     {
